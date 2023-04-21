70bbfa770a090e06e707b303bc585b40e92ff647
Alberto Bonacina <alberto.bonacina@soluzione1.it>
2023-04-21 23:15:30 +0200

    fix: Rimosso async inutile

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soluzione1_hackathon_fudeo_flutter/app/app.dart';
 
-void main() async {
-  //Nel caso di applicazione lanciata su Web non viene messo # nel path dell'url
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
